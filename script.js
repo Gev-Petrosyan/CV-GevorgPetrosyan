011d7d75d3e501531c9a13bfdb432ac5c57f9671
Hrach2003 <hrachya.hovakimyan.03@mail.ru>
2021-10-01 15:21:06 +0400

    my firtst commit

---
+++ script.js
@@ -0,0 +1,108 @@
+function myFunction() { 
+    var x = document.getElementById("click");
+    if (x.style.width === "35%") { 
+      x.style.width = "20%";
+      x.style.transition = "transform 0.5s ease";
+    } else {
+      x.style.width = "35%";
+      x.style.transition = "transform 0.5s ease";
+    }
+
+  } 
+
+  function myFunction2() {
+    var x = document.getElementById("click2");
+    if (x.style.width === "35%") {
+      x.style.width = "20%";
+      x.style.transition = "transform 0.5s ease";
+    } else {
+      x.style.width = "35%";
+      x.style.transition = "transform 0.5s ease";
+    }
+
+  } 
+
+  function myFunction3() {
+    var x = document.getElementById("click3");
+    if (x.style.width === "35%") {
+      x.style.width = "20%";
+      x.style.transition = "transform 0.5s ease";
+    } else {
+      x.style.width = "35%";
+      x.style.transition = "transform 0.5s ease";
+    }
+
+  } 
+
+  function myFunction4() {
+    var x = document.getElementById("click4");
+    if (x.style.width === "35%") {
+      x.style.width = "20%";
+      x.style.transition = "transform 0.5s ease";
+    } else {
+      x.style.width = "35%";
+      x.style.transition = "transform 0.5s ease";
+    }
+
+  } 
+
+  function myFunction5() {
+    var x = document.getElementById("click5");
+    if (x.style.width === "35%") {
+      x.style.width = "20%";
+      x.style.transition = "transform 0.5s ease";
+    } else {
+      x.style.width = "35%";
+      x.style.transition = "transform 0.5s ease";
+    }
+
+  } 
+
+  function myFunction6() {
+    var x = document.getElementById("click6");
+    if (x.style.width === "35%") {
+      x.style.width = "20%";
+      x.style.transition = "transform 0.5s ease";
+    } else {
+      x.style.width = "35%";
+      x.style.transition = "transform 0.5s ease";
+    }
+
+  } 
+
+  function myFunction7() {
+    var x = document.getElementById("click7");
+    if (x.style.width === "35%") {
+      x.style.width = "20%";
+      x.style.transition = "transform 0.5s ease";
+    } else {
+      x.style.width = "35%";
+      x.style.transition = "transform 0.5s ease";
+    }
+
+  } 
+
+  function myFunction8() {
+    var x = document.getElementById("click8");
+    if (x.style.width === "35%") {
+      x.style.width = "20%";
+      x.style.transition = "transform 0.5s ease";
+    } else {
+      x.style.width = "35%";
+      x.style.transition = "transform 0.5s ease";
+    }
+
+  } 
+
+  function myFunction9() {
+    var x = document.getElementById("click9");
+    if (x.style.width === "35%") {
+      x.style.width = "20%";
+      x.style.transition = "transform 0.5s ease";
+    } else {
+      x.style.width = "35%";
+      x.style.transition = "transform 0.5s ease";
+    }
+
+  } 
+
